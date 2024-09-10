80108132172fa00a659ab1f6cf01a33d3ac20c58
Matheus Piegas <matheusaugustopiegassales@gmail.com>
2024-09-10 20:19:37 -0300

    aula 01 pt 1

---
+++ src/http/server.ts
@@ -0,0 +1,41 @@
+import fastify from 'fastify'
+import z from 'zod'
+import { createGoal } from '../functions/create-goal'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+
+const app = fastify({
+  logger: true,
+}).withTypeProvider<ZodTypeProvider>()
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.post(
+  '/goals',
+  {
+    schema: {
+      body: z.object({
+        title: z.string(),
+        desiredWeeklyFrequency: z.number().int().min(1).max(7),
+      }),
+    },
+  },
+  async request => {
+    const { desiredWeeklyFrequency, title } = request.body
+    await createGoal({
+      title,
+      desiredWeeklyFrequency,
+    })
+  }
+)
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('Server is running on port 3333')
+  })
