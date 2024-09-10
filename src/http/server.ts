5b452d1c9a2527fa9058bb9c0df4453cad4a4d3a
Matheus Piegas <matheusaugustopiegassales@gmail.com>
2024-09-10 20:34:59 -0300

    aula 01 pt final

--- src/http/server.ts
+++ src/http/server.ts
@@ -6,12 +6,36 @@ import {
   validatorCompiler,
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
+import { getWeekPendingGoals } from '../functions/get-week-pending-goals'
+import { createGoalCompletion } from '../functions/create-goal-completion'
 
 const app = fastify({
   logger: true,
 }).withTypeProvider<ZodTypeProvider>()
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
+
+app.get('/pending-goals', async () => {
+  const { pendingGoals } = await getWeekPendingGoals()
+  return { pendingGoals }
+})
+
+app.post(
+  '/completions',
+  {
+    schema: {
+      body: z.object({
+        goalId: z.string(),
+      }),
+    },
+  },
+  async request => {
+    const { goalId } = request.body
+    await createGoalCompletion({
+      goalId,
+    })
+  }
+)
 
 app.post(
   '/goals',
